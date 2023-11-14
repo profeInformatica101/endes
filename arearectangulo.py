ff6f1caa65dd8d7dc7b41d6c0ed39b669ce4aa31
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2023-11-14 12:51:38 +0100

    Delete arearectangulo.py

--- arearectangulo.py
+++
@@ -1,4 +0,0 @@
-base = float(input("Introduce la base del rectángulo: "))
-altura = float(input("Introduce la altura del rectángulo: "))
-area = base * altura
-print(f"El área del rectángulo es: {area}")
