ebad89a34c915d22d0ebe5b27945b6275cd3c1dd
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2024-01-16 13:05:38 +0000

    Add files via upload

---
+++ ra3/debugger/Ejemplo2.java
@@ -0,0 +1,29 @@
+package com.ejemplos.debugger;
+
+public class Ejemplo2 {
+	public static void main(String[] args) {
+		int n = 5;
+		int[] tabla = new int[n];
+		
+		tabla = llenar(n);
+		int suma = sumar(tabla);
+		
+		System.out.println("La suma es: " + suma);
+	}
+
+	private static int[] llenar(int n) {
+			int[] tabla = new int[n];
+			for(int i = 0; i < n; i++) {
+				tabla[i] = i * 10;
+			}
+		return tabla;
+	}//llenar tabla
+	
+	private static int sumar(int[] tabla) {
+		int suma = 0;
+		int n=tabla.length;
+		for(int i = 0; i < n; i++) 
+			suma = suma + tabla[i];
+		return suma;
+	}
+}
